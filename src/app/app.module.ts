613f37d537ff5872165a47cdf44cd670e75dca9c
Raven737 <prvm79@gmail.com>
2024-11-21 18:23:13 +0200

    Redux visualization in browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Для роботи з ngModel
+import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { todoReducer } from './state/todo.reducer';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,10 @@ import { todoReducer } from './state/todo.reducer';
     BrowserModule,
     FormsModule, // Для обробки форми
     StoreModule.forRoot({ todos: todoReducer }), // Реєструємо ред'юсер
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Кількість збережених станів
+      logOnly: environment.production, // Лише перегляд у продакшн-режимі
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
